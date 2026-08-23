38b76c756f994a8bc278e8713f8d2c3c2c87918c
RIngoame196 <132573268+Ringoame196@users.noreply.github.com>
2026-08-23 15:23:30 +0900

    feat 二段ジャンプ サウンド、パーティクル実装

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/DoubleJumpBoots.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/DoubleJumpBoots.kt
@@ -1,6 +1,8 @@
 package com.github.ringoame196_s_mcPlugin
 
 import org.bukkit.Material
+import org.bukkit.Particle
+import org.bukkit.Sound
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 
@@ -16,5 +18,15 @@ class DoubleJumpBoots : JumpItem, ToggleSneak {
         if (!isSneaking) return
         if (player.isGrounded) return
         player.jump()
+        playJumpEffect(player)
+    }
+
+    fun playJumpEffect(player: Player) {
+        val world = player.world
+        val location = player.location
+        val particle = Particle.CLOUD
+        val sound = Sound.ENTITY_BAT_TAKEOFF
+        world.spawnParticle(particle, location, 15, 0.2, 0.1, 0.2, 0.05)
+        player.playSound(player, sound, 0.8f, 1.5f)
     }
 }
