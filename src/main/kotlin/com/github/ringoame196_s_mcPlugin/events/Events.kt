36d20ff32363d681861cb36008a9e54c68e33455
RIngoame196 <132573268+Ringoame196@users.noreply.github.com>
2026-08-28 20:55:48 +0900

    feat 無限ジャンプ対策実装

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/events/Events.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/events/Events.kt
@@ -1,10 +1,14 @@
 package com.github.ringoame196_s_mcPlugin.events
 
+import com.github.ringoame196_s_mcPlugin.DoubleJumpManager
 import com.github.ringoame196_s_mcPlugin.JumpItem
 import com.github.ringoame196_s_mcPlugin.ToggleSneak
+import com.github.ringoame196_s_mcPlugin.isGrounded
 import com.github.ringoame196_s_mcPlugin.jump
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerMoveEvent
 import org.bukkit.event.player.PlayerToggleSneakEvent
 
 class Events(jumpItems: List<JumpItem>) : Listener {
@@ -16,6 +20,27 @@ class Events(jumpItems: List<JumpItem>) : Listener {
         val boots = player.inventory.boots ?: return
         val jumpItem = jumpItemMap[boots.itemMeta.jump.id] ?: return
         if (jumpItem !is ToggleSneak) return
+        if (DoubleJumpManager.hasJumped(player)) {
+            sendCancelJump(player)
+            return
+        }
+
         jumpItem.onPlayerToggleSneak(player, e.isSneaking)
+
+        DoubleJumpManager.setJumped(player, true)
+    }
+
+    private fun sendCancelJump(player: Player) {
+        val message = "ジャンプできません"
+        player.sendMessage(message)
+    }
+
+    @EventHandler
+    fun onMove(e: PlayerMoveEvent) {
+        val player = e.player
+        if (!DoubleJumpManager.hasJumped(player)) return
+        if (player.isGrounded) {
+            DoubleJumpManager.setJumped(player, false)
+        }
     }
 }
