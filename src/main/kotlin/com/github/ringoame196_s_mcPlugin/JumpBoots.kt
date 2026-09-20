d3883e7579c9445e641230a44a62fd122fce80e7
RIngoame196 <132573268+Ringoame196@users.noreply.github.com>
2026-09-20 16:31:02 +0900

    chore: メッセージ表示をインターフェースに移動

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/JumpBoots.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/JumpBoots.kt
@@ -1,5 +1,9 @@
 package com.github.ringoame196_s_mcPlugin
 
+import com.github.ringoame196_s_mcPlugin.message.MessageKey
+import com.github.ringoame196_s_mcPlugin.message.MessageManager
+import net.md_5.bungee.api.ChatMessageType
+import net.md_5.bungee.api.chat.TextComponent
 import org.bukkit.Color
 import org.bukkit.Particle
 import org.bukkit.Sound
@@ -30,6 +34,20 @@ interface JumpBoots : JumpItem {
         world.spawnParticle(particle, location, particleCount, 0.2, 0.1, 0.2, 0.05)
         player.playSound(player, sound, volume, pitch)
     }
+
+    fun sendJumpSuccess(player: Player, messageManager: MessageManager) {
+        val message = messageManager.get(MessageKey.JUMP_MESSAGE)
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, *TextComponent.fromLegacyText(message))
+    }
+
+    fun sendJumpCancel(player: Player, messageManager: MessageManager) {
+        val message = messageManager.get(MessageKey.NO_CAN_JUMP_MESSAGE)
+        val sound = Sound.BLOCK_NOTE_BLOCK_BELL
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, *TextComponent.fromLegacyText(message))
+        player.playSound(player, sound, 1f, 1f)
+    }
+
+    fun cancel(player: Player) {}
 
     fun isAction(player: Player, e: Event? = null): Boolean = true
     fun isCancel(player: Player, e: Event? = null): Boolean = false
